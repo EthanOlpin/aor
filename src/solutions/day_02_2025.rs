f2e72a4d4a6a0e3b879565ec61695c44a2249629
Ethan Olpin <olpin3@gmail.com>
2025-12-03 19:38:33 -0700

    small optimizations for day 2, 2025

--- src/solutions/day_02_2025.rs
+++ src/solutions/day_02_2025.rs
@@ -1,24 +1,19 @@
 use std::collections::HashSet;
 
 use aor::solution::solution_main;
+use itertools::Itertools;
 
 /// Day 2, 2025 | https://adventofcode.com/2025/day/2
 
-fn parse_ranges(input: String) -> Vec<(u64, u64)> {
+fn parse_ranges(input: &str) -> impl Iterator<Item = (u64, u64)> {
     input
-        .split(',')
-        .map(|line| {
-            let (start_str, end_str) = line.split_once('-').unwrap();
-            let start = start_str.trim().parse::<u64>().unwrap();
-            let end = end_str.trim().parse::<u64>().unwrap();
-            (start, end)
-        })
-        .collect()
+        .split([',', '-'])
+        .map(|num| num.parse::<u64>().unwrap())
+        .tuples()
 }
 
 fn part1(input: String) -> anyhow::Result<String> {
-    let mut ranges = parse_ranges(input);
-    ranges.sort_unstable();
+    let ranges = parse_ranges(&input).sorted_unstable();
     let lower_bound = |i: u32| -> u64 { 10u64.pow(i) + 10u64.pow(i / 2) };
     let upper_bound = |i: u32| -> u64 { 10u64.pow(i + 1) - 1 };
 
@@ -73,13 +68,11 @@ fn split_range_by_digits(lo: u64, hi: u64) -> Vec<(u64, u64)> {
 }
 
 fn part2(input: String) -> anyhow::Result<String> {
-    let mut ranges = parse_ranges(input)
-        .into_iter()
+    let ranges = parse_ranges(&input)
+        .sorted_unstable()
         .flat_map(|(lo, hi)| split_range_by_digits(lo, hi))
-        .filter(|(lo, _)| lo >= &10)
-        .collect::<Vec<_>>();
+        .filter(|(lo, _)| lo >= &10);
 
-    ranges.sort_unstable();
     let mut invalid_ids = HashSet::new();
 
     for (lo, hi) in ranges {
@@ -109,22 +102,22 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let input = r#"11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
-1698522-1698528,446443-446449,38593856-38593862,565653-565659,
-824824821-824824827,2121212118-2121212124"#
+        let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,\
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,\
+824824821-824824827,2121212118-2121212124"
             .to_string();
-        let expected_output = r#"1227775554"#.to_string();
+        let expected_output = "1227775554".to_string();
         let result = part1(input).unwrap();
         assert_eq!(result, expected_output);
     }
 
     #[test]
     fn test_part2() {
-        let input = r#"11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
-1698522-1698528,446443-446449,38593856-38593862,565653-565659,
-824824821-824824827,2121212118-2121212124"#
+        let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,\
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,\
+824824821-824824827,2121212118-2121212124"
             .to_string();
-        let expected_output = r#"4174379265"#.to_string();
+        let expected_output = "4174379265".to_string();
         let result = part2(input).unwrap();
         assert_eq!(result, expected_output);
     }
